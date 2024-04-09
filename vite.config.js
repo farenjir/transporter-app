84389e914d5e6abd5f81473a0f7cd63b6e52397e
Farshid <farenjir@gmail.com>
2024-04-10 01:21:13 +0330

    jsconfig_initialize-with-vite_pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react-swc";
 import jsconfigPaths from "vite-jsconfig-paths";
 
 import { VitePWA } from "vite-plugin-pwa";
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
